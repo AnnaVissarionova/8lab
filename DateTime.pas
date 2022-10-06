a46e0a7a701a3608ed7c8c595deb91cbf70b9e9e
Виссарионова Анна <anna.ogneva.455@mail.ru>
2022-10-06 11:16:39 +0300

    update DateTime.pas

--- DateTime.pas
+++ DateTime.pas
@@ -1,15 +1,14 @@
 ﻿begin
    var year:=ReadInteger('Введите год');
    var mnt:=ReadInteger('Введите количество минут');
-   var day1:=ReadInt('Введите 1 день');
-   var day2:=ReadInt('Введите 2 день');
-   var days1, days2:= (0,0);
+   var day1:=ReadInteger('Введите 1 день');
+   var day2:=ReadInteger('Введите 2 день');
    if ((year mod 4 = 0) and (year mod 100 <> 0)) or (year mod 400 = 0) then
      Println($'{year} год високосный')
    else 
   Println($'{year} год невисокосный');
   Println($'{mnt * 60} секунд в {mnt} минутах');
-  if day1 > day2 then Print($'{day1} ближе') ;
-  else  Print($'{day2} ближе') ;
+  if day1 > day2 then Print($'{day1} ближе') 
+  else  Print($'День {day2} ближе') ;
   
 end.
