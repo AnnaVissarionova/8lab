eca326f42903cadf4eff629e1a5cf478a81b4011
Виссарионова Анна <anna.ogneva.455@mail.ru>
2022-10-06 11:04:51 +0300

    add Apoc.pas and SecInHrs.pas

---
+++ Apoc.pas
@@ -0,0 +1,7 @@
+﻿begin
+   var year:=ReadInteger('Введите год');
+   if (year = 1992)or (year = 2005)or (year = 2011) then 
+     Print($'В {year} году был апокалипсис')
+   else
+     Print($'В {year}  году не было апокалипсиса');
+end.
---
+++ SecInHrs.pas
@@ -0,0 +1,4 @@
+﻿begin
+  var hrs:=ReadInteger('Введите количество часов');
+  Print(hrs * 3600);
+end.
