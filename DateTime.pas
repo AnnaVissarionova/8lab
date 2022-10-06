e975336803705c9bb55ee15db84f3e9bc39c9f04
Виссарионова Анна <anna.ogneva.455@mail.ru>
2022-10-06 10:29:16 +0300

    update DateTime.pas

--- DateTime.pas
+++ DateTime.pas
@@ -1,3 +1,10 @@
 ﻿begin
-
-end
+   var year:=ReadInteger('Введите год');
+   var mnt:=ReadInteger('Введите количество минут');
+   if ((year mod 4 = 0) and (year mod 100 <> 0)) or (year mod 400 = 0) then
+     Println($'{year} год високосный')
+   else 
+  Println($'{year} год невисокосный');
+  Print($'{mnt * 60} секунд в {mnt} минутах');
+  
+end.
