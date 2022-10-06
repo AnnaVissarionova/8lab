ddc7f7ac914715da78544bb6b14a25fb267cc49c
Виссарионова Анна <anna.ogneva.455@mail.ru>
2022-10-06 10:45:45 +0300

    add CountOfDays.pas

---
+++ CountOfDays.pas
@@ -0,0 +1,6 @@
+﻿begin
+  var year:=ReadInteger('Введите год');
+  if ((year mod 4 = 0) and (year mod 100 <> 0)) or (year mod 400 = 0) then
+    Print($'366 дней в {year} году');
+  else Print($'365 дней в {year} году');
+end.
--- DateTime.pas
+++ DateTime.pas
@@ -1,10 +1,15 @@
 ﻿begin
    var year:=ReadInteger('Введите год');
    var mnt:=ReadInteger('Введите количество минут');
+   var day1:=ReadInt('Введите 1 день');
+   var day2:=ReadInt('Введите 2 день');
+   var days1, days2:= (0,0);
    if ((year mod 4 = 0) and (year mod 100 <> 0)) or (year mod 400 = 0) then
      Println($'{year} год високосный')
    else 
   Println($'{year} год невисокосный');
-  Print($'{mnt * 60} секунд в {mnt} минутах');
+  Println($'{mnt * 60} секунд в {mnt} минутах');
+  if day1 > day2 then Print($'{day1} ближе') ;
+  else  Print($'{day2} ближе') ;
   
 end.
