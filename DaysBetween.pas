c440220b4a01f31138646d8a909dd9265861f473
Виссарионова Анна <anna.ogneva.455@mail.ru>
2022-10-06 10:53:06 +0300

    update .pas

--- DaysBetween.pas
+++ DaysBetween.pas
@@ -2,8 +2,8 @@
   var year1:=ReadInteger('Введите 1 год');
   var year2:=ReadInteger('Введите 2 год');
   var count:=0;
-  for var i:year1 to year2 do begin
-    if ((year mod 4 = 0) and (year mod 100 <> 0)) or (year mod 400 = 0) then
+  for var i:=year1 to year2 do begin
+    if ((i mod 4 = 0) and (i mod 100 <> 0)) or (i mod 400 = 0) then
       count+=366
     else 
       count+=365
